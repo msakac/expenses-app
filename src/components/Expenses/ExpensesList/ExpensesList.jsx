d5088d3150c6fb75bce82936b02e573495fb2f7e
Martin Sakac <msakac@foi.hr>
2022-07-22 10:33:53 +0200

    Overcomplicating

---
+++ src/components/Expenses/ExpensesList/ExpensesList.jsx
@@ -0,0 +1,15 @@
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import './ExpensesList.css'
+
+function ExpensesList({expenses}){
+
+    if(expenses.length === 0){
+        return <p>No expenses found!</p>;
+    }
+
+    return <ul className='expenses-list'>
+        {expenses.map(expense => <ExpenseItem key={expense.id} expense={expense} />)}
+    </ul>
+}
+
+export default ExpensesList;
