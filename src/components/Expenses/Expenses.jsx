6905aee2cad8af6f472a3aa86ec7b5d69f8ec253
Martin Sakac <msakac@foi.hr>
2022-07-22 10:34:07 +0200

    Overcomplicating #2

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,26 +3,25 @@ import Card from '../Card/Card'
 import ExpenseItem from './ExpenseItem/ExpenseItem'
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
+import ExpensesList from './ExpensesList/ExpensesList'
 
 function Expenses({ expenses }) {
     const [filteredYear, setFilteredYear] = useState('2020')
 
-        const filterChangeHandler = (selectedYear) =>{
-            setFilteredYear(selectedYear)
-        };
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    };
 
-        const filterArray = expenses.filter((expense) => {
-            return expense.date.getFullYear().toString() === filteredYear;
-        });
+    const filteredExpenses = expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
-
+    
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter filteredYear={filteredYear} onChangeFilter={filterChangeHandler} />
-                {
-                    filterArray.map(expense => <ExpenseItem key={expense.id} expense={expense} />)
-                }
+                    <ExpensesList expenses={filteredExpenses}/>
             </Card>
         </div>
     )
