3a8f80f4c3ec6d4db19e221f846d3776bb549452
Martin Sakac <msakac@foi.hr>
2022-07-22 08:12:22 +0200

    NewExpense and ExpenseForm component

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm/ExpenseForm';
 import './NewExpense.css';
 
 function NewExpense() {
     return <div className='new-expense'>
-
+        <ExpenseForm/>
     </div>
 }
 
